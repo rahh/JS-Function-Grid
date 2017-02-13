3c71c29e58448e7887a9a707dc4e8cc0eb6a2e83
Rafat <refatc16@yahoo.com>
2017-02-13 15:15:51 +0000

    smile

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ setColor(0, 0, "green");
 setColor(0,2,"blue");
 setColor(0,4,"orange");
 setColor(0,6,"red");
+
+setColor(4,3,"gray");
+setColor(4,7,"gray");
+setColor(7,1,"black");
+setColor(8,2,"black");
+setColor(9,3,"black");
+setColor(9,4,"black");
+setColor(9,5,"black");
+setColor(9,6,"black");
+setColor(8,7,"black");
+setColor(7,8,"black");
